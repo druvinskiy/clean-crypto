bde9a40f801d05159d8295868b48db97be553a69
David Ruvinskiy <david.ruvinskiy@sbcglobal.net>
2022-07-27 21:32:19 -0500

    Inital commit

---
+++ CleanCrypto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanCrypto
+//
+//  Created by David Ruvinskiy on 7/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
